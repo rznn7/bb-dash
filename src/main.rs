b5af9772fe3d04644374ae561c89b03fe56f800f
rznn7 <160644362+rznn7@users.noreply.github.com>
2026-01-16 12:43:50 +0100

    setup log with tracing

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,22 @@
 #![allow(dead_code)]
 
-use crate::tui::App;
+use tracing::info;
+
+use crate::{logging::initialize_logging, tui::App};
 use std::env;
 
 mod bitbucket_api;
 mod bitbucket_client;
 mod bitbucket_repo;
+mod logging;
 mod tui;
 
 #[tokio::main]
 async fn main() -> Result<(), anyhow::Error> {
+    initialize_logging()?;
+
+    info!("app started");
+
     let repo_path = env::args().nth(1);
 
     let terminal = ratatui::init();
